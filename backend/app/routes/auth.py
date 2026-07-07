e59f830cb56c564727332f6042432f1121a09266
Naman Goyal <namangoyal874n@gmail.com>
2026-07-07 19:25:26 +0530

    Implement user signup with PostgreSQL

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -1,17 +1,33 @@
 from fastapi import APIRouter
+from app.schemas.user import UserCreate
+from app.models.user import User
+from app.database.database import SessionLocal
 
 router = APIRouter()
+
+
+@router.post("/signup")
+def signup(user: UserCreate):
+    db = SessionLocal()
+
+    new_user = User(
+        full_name=user.full_name,
+        email=user.email,
+        password=user.password
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    db.close()
+
+    return {
+        "message": "User created successfully"
+    }
 
 
 @router.post("/login")
 def login():
     return {
-        "message": "Login API Working"
-    }
-
-
-@router.post("/signup")
-def signup():
-    return {
-        "message": "Signup API Working"
+        "message": "Login Working"
     }
